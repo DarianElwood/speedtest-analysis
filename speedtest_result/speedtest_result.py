7bf12c9af432139797f34b57640006214ae0a0b1
Darian Elwood <delwood@academic.rrc.ca>
2025-10-07 15:57:37 -0500

    Add documentation

--- speedtest_result/speedtest_result.py
+++ speedtest_result/speedtest_result.py
@@ -1,7 +1,25 @@
 """Module defining the SpeedTestResult class."""
 
 class SpeedTestResult:
-    """Helper class to store individual speed test results."""
+    """Helper class to store individual speed test results.
+    Args: 
+        server (str): The server used for the speed test.
+        ping (float): The ping result in milliseconds.
+        download (float): The download speed in Mbps.
+        upload (float): The upload speed in Mbps.
+        device (str): The device on which the test was conducted.
+    Attributes:
+        server (str): The server used for the speed test.
+        ping (float): The ping result in milliseconds.
+        download (float): The download speed in Mbps.
+        upload (float): The upload speed in Mbps.
+        device (str): The device on which the test was conducted.
+    Methods:
+        __init__: Initializes a SpeedTestResult instance with the 
+        provided parameters.
+        
+    """
+    
     def __init__(self,
                  server: str,
                  ping: float,
